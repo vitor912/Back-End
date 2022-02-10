a5f10e4a1279c4e203b4ad35d16449eaeb89e5d6
Vitor Ribeiro <97813061+vitor912@users.noreply.github.com>
2022-02-10 19:26:44 -0300

    Iniciando Ex_Fixação_02

--- Projetos em C#/Ex_Fixação_02/Ex_Fixação_02/Program.cs
+++ Projetos em C#/Ex_Fixação_02/Ex_Fixação_02/Program.cs
@@ -1,12 +1,29 @@
 ﻿using System;
+using System.Globalization;
 
-namespace Ex_Fixação_02
-{
-    internal class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
+namespace Ex_Fixação_02 {
+    internal class Program {
+        static void Main(string[] args) {
+
+            Console.WriteLine("Entre com seu nome completo:");
+            string name = Console.ReadLine();
+            Console.WriteLine();
+            Console.WriteLine("Entre com sua idade:");
+            int age = int.Parse(Console.ReadLine());
+            Console.WriteLine();
+            Console.WriteLine("Entre com sua altura:");
+            double h1 = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.WriteLine();
+            Console.WriteLine("Entre com  o seu peso:");
+            double weight = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.WriteLine();
+            Console.WriteLine("Qual é o seu sexo (M) Masculino (F) Feminino");
+            char sex = char.Parse(Console.ReadLine());
+            Console.WriteLine();
+            Console.WriteLine("Me diga uma cor, altura e comprimento");
+
+
+            
         }
     }
 }
