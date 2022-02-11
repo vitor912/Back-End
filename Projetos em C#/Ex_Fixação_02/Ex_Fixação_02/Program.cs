a3a992c9ce8700fb6411e6305519db952c2c3ac8
Vitor Ribeiro <97813061+vitor912@users.noreply.github.com>
2022-02-11 17:54:55 -0300

    Finalizando Ex_FIxação_02

--- Projetos em C#/Ex_Fixação_02/Ex_Fixação_02/Program.cs
+++ Projetos em C#/Ex_Fixação_02/Ex_Fixação_02/Program.cs
@@ -21,9 +21,22 @@ namespace Ex_Fixação_02 {
             char sex = char.Parse(Console.ReadLine());
             Console.WriteLine();
             Console.WriteLine("Me diga uma cor, altura e comprimento");
-
-
-            
+            string[] vs = Console.ReadLine().Split(' ');
+            string color = vs[0];
+            double h2 = double.Parse (vs[1], CultureInfo.InvariantCulture);
+            double length = double.Parse (vs[2], CultureInfo.InvariantCulture);
+            Console.WriteLine();
+            Console.WriteLine("-------------------------------------------------");
+            Console.WriteLine();
+            Console.WriteLine("Seus dados são");
+            Console.WriteLine(name);
+            Console.WriteLine(age);
+            Console.WriteLine(h1.ToString("F2",CultureInfo.InvariantCulture));
+            Console.WriteLine(weight.ToString("F2",CultureInfo.InvariantCulture));
+            Console.WriteLine(sex);
+            Console.WriteLine(color);
+            Console.WriteLine(h2.ToString("F2",CultureInfo.InvariantCulture));
+            Console.WriteLine(length.ToString("F2",CultureInfo.InvariantCulture));
         }
     }
 }
