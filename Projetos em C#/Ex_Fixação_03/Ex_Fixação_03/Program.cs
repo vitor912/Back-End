1ddf2bcdd4293768a428ed73ba596a416bf8f16e
Vitor Ribeiro <97813061+vitor912@users.noreply.github.com>
2022-02-24 18:05:09 -0300

    Fim Ex_Fixação_03

---
+++ Projetos em C#/Ex_Fixação_03/Ex_Fixação_03/Program.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace Ex_Fixação_03 {
+    internal class Program {
+        static void Main(string[] args) {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
