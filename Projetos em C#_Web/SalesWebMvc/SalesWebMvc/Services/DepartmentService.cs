38b13cf86db87be687bba2491fc0b2e464fbb115
Vitor Ribeiro <dev_vitor912@hotmail.com>
2022-06-13 17:45:55 -0300

    Inicializando

---
+++ Projetos em C#_Web/SalesWebMvc/SalesWebMvc/Services/DepartmentService.cs
@@ -0,0 +1,22 @@
+﻿using SalesWebMvc.Models;
+using SalesWebMvc.Data;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace SalesWebMvc.Services
+{
+    public class DepartmentService
+    {
+        private readonly SalesWebMvcContext _context;
+
+        public DepartmentService(SalesWebMvcContext context)
+        {
+            _context = context;
+        }
+
+        public List<Departamento> FindAll()
+        {
+            return _context.Departamento.OrderBy(x => x.Nome).ToList();
+        }
+    }
+}
