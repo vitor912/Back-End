b3807d43328fdc7250edf634ce034f794b2fdf45
Vitor Ribeiro <dev_vitor912@hotmail.com>
2022-06-13 17:50:41 -0300

    inicializando TelaDeLoginMvc

---
+++ Projetos em C#_Web/TelaDeLoginMvc/TelaDeLoginMvc/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace TelaDeLoginMvc.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
