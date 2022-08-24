32062add503caf9311347b044aa4ad39b0a7c0cf
Vitor Ribeiro <dev_vitor912@hotmail.com>
2022-08-24 16:37:30 -0300

    Percentage

---
+++ Logica de Programação/Porcentagem/Porcentagem/Program.cs
@@ -0,0 +1,17 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.Write("Qual o valor do empréstimo? ");
+        double emp = double.Parse(Console.ReadLine());
+
+        double por = emp * 0.20;
+        double valor = por + emp;
+
+        Console.Write("Quantas parcelas? ");
+        double parcelas = double.Parse(Console.ReadLine());
+
+        double resultado = valor / parcelas;
+        Console.WriteLine($"Vou pagar {parcelas} parcelas de R${resultado} reais");
+    }
+}
