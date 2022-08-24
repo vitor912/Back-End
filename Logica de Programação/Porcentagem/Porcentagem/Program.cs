83e7e3f228ef527ab4ebebc891c32bcaa232bd87
Vitor Ribeiro <dev_vitor912@hotmail.com>
2022-08-24 17:30:04 -0300

    placing the installments

--- Logica de Programação/Porcentagem/Porcentagem/Program.cs
+++ Logica de Programação/Porcentagem/Porcentagem/Program.cs
@@ -1,17 +1,43 @@
-﻿internal class Program
+﻿using System.Globalization;
+
+internal class Program
 {
     private static void Main(string[] args)
     {
         Console.Write("Qual o valor do empréstimo? ");
         double emp = double.Parse(Console.ReadLine());
 
-        double por = emp * 0.20;
-        double valor = por + emp;
-
         Console.Write("Quantas parcelas? ");
         double parcelas = double.Parse(Console.ReadLine());
 
-        double resultado = valor / parcelas;
-        Console.WriteLine($"Vou pagar {parcelas} parcelas de R${resultado} reais");
+
+        if (parcelas <= 4)
+        {
+            double porc = emp * 0.08;
+            double valorPorc = emp + porc;
+            double resultado = valorPorc / parcelas;
+            Console.WriteLine($"Vou pagar {parcelas} parcelas de R${resultado.ToString("F2", CultureInfo.InvariantCulture)} reais");
+            Console.WriteLine("O total de prestações á pagar: R$" + valorPorc.ToString("F2", CultureInfo.InvariantCulture) + " reais");
+        }
+        else if (parcelas <= 10)
+        {
+            double porc = emp * 0.15;
+            double valorPorc = emp + porc;
+            double resultado = valorPorc / parcelas;
+            Console.WriteLine($"Vou pagar {parcelas} parcelas de R${resultado.ToString("F2", CultureInfo.InvariantCulture)} reais");
+            Console.WriteLine("O total de prestações á pagar: R$" + valorPorc.ToString("F2", CultureInfo.InvariantCulture) + " reais");
+        }
+        else if (parcelas <= 18)
+        {
+            double porc = emp * 0.20;
+            double valorPorc = emp + porc;
+            double resultado = valorPorc / parcelas;
+            Console.WriteLine($"Vou pagar {parcelas} parcelas de R${resultado.ToString("F2", CultureInfo.InvariantCulture)} reais");
+            Console.WriteLine("O total de prestações á pagar: R$" + valorPorc.ToString("F2", CultureInfo.InvariantCulture) + " reais");
+        }
+        else
+        {
+            Console.WriteLine("Parcelamentos até 18x");
+        }
     }
 }
